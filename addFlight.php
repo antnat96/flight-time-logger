10c4f16bbcafed5e00fa8dd162e6fe4c7ed06b60
Anthony N <antnat96@gmail.com>
2020-03-19 12:27:13 -0400

    updated model interactions & row highlighting

--- addFlight.php
+++ addFlight.php
@@ -88,6 +88,12 @@
                   <input type="time" class="form-control" id="arrivalTimeZulu" placeholder = "1100">
                 </div>
               </div>
+              <div class = "form-row">
+                <div class="form-group col-md-12">
+                  <label for="flightTime">Flight Time</label>
+                  <input type="number" class="form-control" id="flightTime" placeholder = "8.5">
+                </div>              
+              </div>
             </form>
             <br>
             <form id = "cargo-info" class = "pop flight-form">
@@ -127,85 +133,90 @@
     </div>
     <script type = "text/javascript">
       $(document).ready(function() {
-          
-          // Handler for the "Add Flight" button
-          $("#add-flight-button").on("click", function() {
+        
+        // Handler for the "Add Flight" button
+        $("#add-flight-button").on("click", function() {
 
-            // Getting input elements & values
-            var aircraftInfoRaw = document.querySelectorAll("#aircraft-info input");
-            var flightInfoRaw = document.querySelectorAll("#flight-info input");
-            var cargoInfoRaw = document.querySelectorAll("#cargo-info input");
+          // Generate random 5 character alphanumeric identifier string
+          let randomString = Math.random().toString(36).slice(2).substr(0, 5);
 
-            // Initializing value arrays
-            var aircraftInfo = [];
-            var flightInfo = [];
-            var cargoInfo = [];
+          // Getting input elements & values
+          var aircraftInfoRaw = document.querySelectorAll("#aircraft-info input");
+          var flightInfoRaw = document.querySelectorAll("#flight-info input");
+          var cargoInfoRaw = document.querySelectorAll("#cargo-info input");
 
-            // Filling value arrays
-            for (var i = 0, element; element = aircraftInfoRaw[i++];) {
-                if (element == null || element == undefined || element == "") {
-                  aircraftInfo.push(null);
-                }
-                else {
-                  aircraftInfo.push(element.value);
-                }
-            }
-            for (var i = 0, element; element = flightInfoRaw[i++];) {
+          // Initializing value arrays
+          var aircraftInfo = [];
+          var flightInfo = [];
+          var cargoInfo = [];
+
+          // Filling value arrays
+          for (var i = 0, element; element = aircraftInfoRaw[i++];) {
               if (element == null || element == undefined || element == "") {
-                flightInfo.push(null);
+                aircraftInfo.push(null);
               }
               else {
-                flightInfo.push(element.value);
+                aircraftInfo.push(element.value);
               }
+          }
+          for (var i = 0, element; element = flightInfoRaw[i++];) {
+            if (element == null || element == undefined || element == "") {
+              flightInfo.push(null);
             }
-            for (var i = 0, element; element = cargoInfoRaw[i++];) {
-              if (element == null || element == undefined || element == "") {
-                cargoInfo.push(null);
-              }
-              else {
-                cargoInfo.push(element.value);
-              }
+            else {
+              flightInfo.push(element.value);
             }
+          }
+          for (var i = 0, element; element = cargoInfoRaw[i++];) {
+            if (element == null || element == undefined || element == "") {
+              cargoInfo.push(null);
+            }
+            else {
+              cargoInfo.push(element.value);
+            }
+          }
 
-            let comprehensiveInfo = {        
-              "aircraft": {
-                "type": aircraftInfo[0],
-                "tail_num": aircraftInfo[1]
-              },
-              "departure" : {
-                "date": flightInfo[0],
-                "location": flightInfo[1],
-                "airport_code": flightInfo[2],
-                "time_local": flightInfo[3],
-                "time_zulu": flightInfo[4],
-              },
-              "arrival" : {
-                "date": flightInfo[5],
-                "location": flightInfo[6],
-                "airport_code": flightInfo[7],
-                "time_local": flightInfo[8],
-                "time_zulu": flightInfo[9],
-              },
-              "cargo" : {
-                "num_of_items": cargoInfo[0],
-                "weight_lbs": cargoInfo[1],
-                "weight_kg": cargoInfo[2],
-                "loading_agents": cargoInfo[3],
-                "description": cargoInfo[4]
-              }
-            }
+          let comprehensiveInfo = {
+            "id": randomString,        
+            "aircraft": {
+              "type": aircraftInfo[0],
+              "tail_num": aircraftInfo[1]
+            },
+            "departure" : {
+              "date": flightInfo[0],
+              "location": flightInfo[1],
+              "airport_code": flightInfo[2],
+              "time_local": flightInfo[3],
+              "time_zulu": flightInfo[4],
+            },
+            "arrival" : {
+              "date": flightInfo[5],
+              "location": flightInfo[6],
+              "airport_code": flightInfo[7],
+              "time_local": flightInfo[8],
+              "time_zulu": flightInfo[9],
+            },
+            "time" : flightInfo[10],
+            "cargo" : {
+              "num_of_items": cargoInfo[0],
+              "weight_lbs": cargoInfo[1],
+              "weight_kg": cargoInfo[2],
+              "loading_agents": cargoInfo[3],
+              "description": cargoInfo[4]
+            },
+          }
 
-            $.ajax({
-                url: "https://jsonbox.io/box_1aa2eecea9560cd80c48",
-                data: JSON.stringify(comprehensiveInfo),
-                type: "POST",
-                dataType: "json",
-                contentType: "application/json"
-            }).done(function(result){
-                console.log(result);
-            }).fail(function(result) {
-                console.log(result);
-            });
+          $.ajax({
+              url: "https://jsonbox.io/box_1aa2eecea9560cd80c48",
+              data: JSON.stringify(comprehensiveInfo),
+              type: "POST",
+              dataType: "json",
+              contentType: "application/json"
+          }).done(function(result){
+              console.log(result);
+          }).fail(function(result) {
+              console.log(result);
+          });
         })
       });
     </script>
--- index.php
+++ index.php
@@ -27,6 +27,7 @@
             <table id = "flights_table" class="table table-striped pop">
               <thead>
                 <tr>
+                  <th scope="col">ID</th>
                   <th scope="col">Departure Date</th>
                   <th scope="col">Departure Location</th>
                   <th scope="col">Arrival Date</th>
@@ -45,64 +46,70 @@
       </div>
       <div class = "row">
         <div class = "col-md-12">
-          <p id = "test-paragraph"></p>
+          <button id = "delete-all-button" class="btn btn-primary del-flt-btn">Delete All Flights</button>
         </div>
       </div>
     </div>
     <script type = "text/javascript">
       $(document).ready(function() {
+
+        // Get the flights
         $.ajax({
           url: "https://jsonbox.io/box_1aa2eecea9560cd80c48",
           type: "GET",
           dataType: "json",
           contentType: "application/json"
         }).done(function(result){
-          console.log(result);
           populateTable(result);
+
+          $("td").hover(function() {
+            let nearestRow = $(this).closest("tr");
+            nearestRow.attr("style", "background-color: white")
+          },function() {
+            let nearestRow = $(this).closest("tr");
+            nearestRow.attr("style", "")
+          });
+
         }).fail(function(result) {
-          console.log("Failed " + result);
+          window.alert("There was a problem getting the flights, try again later!");
         });
 
-
+        // Populate the flights table
         function populateTable(info) {
 
+          // if the info array has content
           if (info != undefined && info != null && info[0] != []) {
+
+            // Initialize relevant info array for filling out content in cells
+            let relevantInfo = [];
+
+            // Set the table element
+            let table = document.getElementById("flights_table");
+            
+            // For each flight
             for (let i = 0; i < info.length; i++) {
 
-              let table = document.getElementById("flights_table");
+              // Push relevant info from that flight to the relevantInfo array
+              relevantInfo.push(info[i].id,info[i].departure.date,info[i].departure.location,info[i].arrival.date,info[i].arrival.location,info[i].time,info[i].cargo.weight_lbs,info[i].aircraft.type,info[i].aircraft.tail_num);
+
+              // Add a row
               let newRow = table.insertRow(1);
 
-              let departureDate = info[i].departure.date;
-                let departureDateCell = newRow.insertCell(0);
-                departureDateCell.innerHTML = departureDate;
+              // Initialize the cell variable
+              let cell;
 
-              let departureLocation = info[i].departure.location;
-                let departureLocationCell = newRow.insertCell(1);
-                departureLocationCell.innerHTML = departureLocation;
+              // For each piece of relevant info
+              for (let j = 0; j < relevantInfo.length; j++) {
 
-              let arrivalDate = info[i].arrival.date;
-                let arrivalDateCell = newRow.insertCell(2);
-                arrivalDateCell.innerHTML = arrivalDate;
+                // Add a cell
+                cell = newRow.insertCell(j);
 
-              let arrivalLocation = info[i].arrival.location;
-                let arrivalLocationCell = newRow.insertCell(3);
-                arrivalLocationCell.innerHTML = arrivalLocation;
+                // And fill it
+                cell.innerHTML = relevantInfo[j];
+              }
 
-              let flightTime = "8.5 hr";
-                let flightTimeCell = newRow.insertCell(4);
-                flightTimeCell.innerHTML = flightTime;
-
-              let cargoWtLbs = info[i].cargo.weight_lbs;
-                let cargoWtLbsCell = newRow.insertCell(5);
-                cargoWtLbsCell.innerHTML = cargoWtLbs;
-
-              let aircraftType = info[i].aircraft.type;
-                let aircraftTypeCell = newRow.insertCell(6);
-                aircraftTypeCell.innerHTML = aircraftType;
-
-              let tailNumber = info[i].aircraft.tail_num;
-                let tailNumberCell = newRow.insertCell(7);
-                tailNumberCell.innerHTML = tailNumber;
+              // Unset the relevantInfo array so it can be refilled on next loop
+              relevantInfo = [];
 
             }
           }
@@ -111,6 +118,22 @@
           }
         }
 
+        // Delete flights from the JSON box
+        $(".del-flt-btn").on("click", function() {
+          let id = "deyk6";
+
+          $.ajax({
+            url: "https://jsonbox.io/box_1aa2eecea9560cd80c48?q=id:" + id,
+            type: "DELETE",
+            dataType: "json",
+            contentType: "application/json"
+          }).done(function(result){
+            console.log(result);
+          }).fail(function(result) {
+            console.log(result);
+          });
+
+        })
         // Handle the "Delete" button
         // $(document).on('click', '.remove-student-button', function(){
         //     // Get the closest row element's index and delete it
